4a05bf57792b6fc05a0dc6fceb9ca5f6bc3b4279
eltonMessiass <eltonmessias10@gmail.com>
2024-07-31 23:45:17 +0200

    feat:get all articles

--- blogging_platform_API/blog/views.py
+++ blogging_platform_API/blog/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from  rest_framework.views import APIView
-
+from .models import Article
+from .serializers import ArticleSerializer
+from rest_framework.response import Response
+from rest_framework import status
 
 def index(request):
     return HttpResponse("Hello")
 
-class ArticleView(request, data)
+class ArticleView(APIView):
+
+    def get(self, request, format=None):
+        articles = Article.objects.all()
+        serializzer = ArticleSerializer(articles, many=True)
+        return Response(serializzer.data, status=status.HTTP_200_OK)
+    
+
+
