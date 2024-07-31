80d030fae90cc90d3a9c2c79df9ea48f4c612cee
eltonMessiass <eltonmessias10@gmail.com>
2024-07-31 22:56:43 +0200

    feat:models

---
+++ blogging_platform_API/blog/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=20)
+
+class Article(models.Model):
+    title = models.CharField(max_length=100, null=False, blank=False)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.title
